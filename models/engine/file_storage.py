0ed22c22307b5c10d9d02a0d15dd3e146126ce14
Salma-Nasser <salma.naser1020@gmail.com>
2024-03-26 14:54:18 -0700

    fixed imports in file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,7 +4,6 @@ import datetime
 import json
 import os
 
-
 class FileStorage:
 
     """Class for storing and retrieving data"""
@@ -25,25 +24,6 @@ class FileStorage:
         with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
             d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
             json.dump(d, f)
-
-    def classes(self):
-        """Returns a dictionary of valid classes and their references"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
-
-        classes = {"BaseModel": BaseModel,
-                   "User": User,
-                   "State": State,
-                   "City": City,
-                   "Amenity": Amenity,
-                   "Place": Place,
-                   "Review": Review}
-        return classes
 
     def reload(self):
         """Reloads the stored objects"""
@@ -93,3 +73,9 @@ class FileStorage:
                          "text": str}
         }
         return attributes
+    def classes(self):
+        """Returns a dictionary of valid classes and their references"""
+        from models.base_model import BaseModel
+
+        classes = {"BaseModel": BaseModel}
+        return classes
